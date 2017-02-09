3cf864ecfb1f8fd8093efc1b0bedfbfc037e8b82
Tek Hudson <tek.hudson@gmail.com>
2017-02-08 18:47:52 -0800

    Remove depricated test

--- quiz_generator_test.rb
+++ quiz_generator_test.rb
@@ -1,8 +1,6 @@
 require 'minitest/autorun'
+require_relative 'main'
 require_relative 'quiz_generator'
 
 class QuizGeneratorTest < Minitest::Test
-  def test_ensure_we_ask_for_number_of_questions
-    assert_output("How many questions would you like to include in the quiz? \n") { main }
-  end
 end
