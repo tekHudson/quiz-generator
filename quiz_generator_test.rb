8817ca9125f29011e62a23df3a915573c816d292
Tek Hudson <tek.hudson@gmail.com>
2017-02-08 17:50:54 -0800

    Test, ask for number of questions

--- quiz_generator_test.rb
+++ quiz_generator_test.rb
@@ -2,4 +2,7 @@ require 'minitest/autorun'
 require_relative 'quiz_generator'
 
 class QuizGeneratorTest < Minitest::Test
+  def ensure_we_ask_for_number_of_questions
+    assert_output("How many questions would you like to include in the quiz? \n") { main }
+  end
 end
