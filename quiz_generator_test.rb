ec71823a7dd735f63044e2f93c983b8111a97818
Tek Hudson <tek.hudson@gmail.com>
2017-02-08 19:23:31 -0800

    Add first test for askign question

--- quiz_generator_test.rb
+++ quiz_generator_test.rb
@@ -3,4 +3,13 @@ require_relative 'main'
 require_relative 'quiz_generator'
 
 class QuizGeneratorTest < Minitest::Test
+  def setup
+    input = StringIO.new("8\n")
+    output = StringIO.new
+    @test = QuizGenerator.new input, output
+  end
+
+  def test_asking_the_question
+    assert_output("How many questions would you like to include in the quiz? \n") { @test.get_num_questions }
+  end
 end
