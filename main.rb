2567b502435719cdd8efa7ad2bbd0750bd6ff040
Tek Hudson <tek.hudson@gmail.com>
2017-02-08 19:04:06 -0800

    Refactor populate_questions to properly populate @questions

--- main.rb
+++ main.rb
@@ -4,3 +4,5 @@ require_relative 'quiz_generator'
 quiz = QuizGenerator.new(STDIN, STDOUT)
 
 num_questions = quiz.get_num_questions
+
+quiz.get_questions_for_quiz(num_questions)
--- quiz_generator.rb
+++ quiz_generator.rb
@@ -5,12 +5,25 @@ class QuizGenerator
     @output_stream = output_stream
     @questions = {}
     populate_questions
+    @questions_for_quiz = []
   end
 
   def get_num_questions
     puts "How many questions would you like to include in the quiz? "
     @output_stream.flush
     get_input
+  end
+
+  def get_questions_for_quiz(num)
+    if (num.to_i % 2) == 0
+      # even
+      strand1 = @questions.select {|q_id, key| key[:strand_id] == 1}
+      strand2 = @questions.select {|q_id, key| key[:strand_id] == 2}
+    else
+      # odd
+    end
+    puts strand1.inspect
+    puts strand2.inspect
   end
 
   private
@@ -23,11 +36,11 @@ class QuizGenerator
   def populate_questions
     CSV.foreach("questions.csv") do |row|
       next unless row[0].to_i > 0
-      @questions[row[4]] = { difficulty: row[5] }
-      @questions[row[4]] = { standard_id: row[2] }
-      @questions[row[4]] = { standard_name: row[3] }
-      @questions[row[4]] = { strand_id: row[0] }
-      @questions[row[4]] = { strand_name: row[1] }
+      @questions[row[4].to_i]= { difficulty: row[5],
+                                 standard_id: row[2],
+                                 standard_name: row[3],
+                                 strand_id: row[0],
+                                 strand_name: row[1] }
     end
   end
 end
