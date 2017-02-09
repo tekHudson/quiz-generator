39c8b0e2841d2e680cbc56c8efe2e05a16f30048
Tek Hudson <tek.hudson@gmail.com>
2017-02-08 19:30:01 -0800

    Trying to get mock working

--- quiz_generator_test.rb
+++ quiz_generator_test.rb
@@ -4,12 +4,18 @@ require_relative 'quiz_generator'
 
 class QuizGeneratorTest < Minitest::Test
   def setup
-    input = StringIO.new("8\n")
-    output = StringIO.new
-    @test = QuizGenerator.new input, output
+    @input = MiniTest::Mock.new
+    @output = MiniTest::Mock.new
+
+    @qg = QuizGenerator.new(@input, @output)
   end
 
   def test_asking_the_question
-    assert_output("How many questions would you like to include in the quiz? \n") { @test.get_num_questions }
+    assert_output("How many questions would you like to include in the quiz? \n") { @qg.get_num_questions }
+  end
+
+  def test_storing_input_correctly
+
+    assert_equal "8", @qg.get_num_questions
   end
 end
