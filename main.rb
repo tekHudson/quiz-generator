dbffbec603839fa51c25c4502fee5b347c09793b
Tek Hudson <tek.hudson@gmail.com>
2017-02-08 18:38:48 -0800

    Move QuizGenerator into a class

---
+++ main.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+require_relative 'quiz_generator'
+
+quiz = QuizGenerator.new(STDIN, STDOUT)
+
+numquiz.get_num_questions_requested
--- quiz_generator.rb
+++ quiz_generator.rb
@@ -1,6 +1,31 @@
-#!/usr/bin/env ruby
-def main
-  puts "How many questions would you like to include in the quiz? "
-end
+require 'csv'
+class QuizGenerator
+  def initialize input_stream, output_stream
+    @input_stream = input_stream
+    @questions = {}
+    build_questions
+  end
 
-main
+  def get_num_questions_requested
+    puts "How many questions would you like to include in the quiz? "
+    get_input
+  end
+
+  private
+
+
+  def get_input
+    @input_stream.gets.chomp if @input_stream.gets.to_i > 0
+  end
+
+  def build_questions
+    CSV.foreach("questions.csv") do |row|
+      next unless row[0].to_i > 0
+      @questions[row[4]] = { difficulty: row[5] }
+      @questions[row[4]] = { standard_id: row[2] }
+      @questions[row[4]] = { standard_name: row[3] }
+      @questions[row[4]] = { strand_id: row[0] }
+      @questions[row[4]] = { strand_name: row[1] }
+    end
+  end
+end
